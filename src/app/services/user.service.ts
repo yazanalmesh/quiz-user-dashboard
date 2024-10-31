bb9298e6bdd79eb0663bbddbba4c8832b7d98a30
Yazan Almesh <142325325+yazanalmesh@users.noreply.github.com>
2024-10-31 19:49:58 +0300

    Update user-service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,36 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { UsersData, UserDetails } from '../models/user.model';
-import { map, tap } from 'rxjs/operators';
-import {environment} from '../environments/environment';
+import { Observable } from 'rxjs';
+import { UsersData } from '../models/user.model';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private baseUrl = environment.baseUrl;
-  private usersCache = new Map<number, UsersData>();
-  private userDetailCache = new Map<string, UserDetails>();
 
   constructor(private http: HttpClient) {}
 
   getUsers(page: number): Observable<UsersData> {
-    if (this.usersCache.has(page)) {
-      return of(this.usersCache.get(page) as UsersData);
-    }
-    return this.http.get<UsersData>(`${this.baseUrl}?page=${page}`).pipe(
-      tap(data => this.usersCache.set(page, data))
-    );
+    return this.http.get<UsersData>(`${this.baseUrl}?page=${page}`);
   }
 
-  getUserById(id: string): Observable<UserDetails> {
-    if (this.userDetailCache.has(id)) {
-      return of(this.userDetailCache.get(id) as UserDetails);
-    }
-    return this.http.get<{ data: UserDetails }>(`${this.baseUrl}/${id}`).pipe(
-      tap(response => this.userDetailCache.set(id, response.data)),
-      map(response => response.data)
-    );
+  getUserById(id: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
   }
 }
+
+
